0305ac1fb466d0fe7e2d125e07f79744dc7b0845
rouamansour <rouamansour.rm@gmail.com>
2021-07-14 23:14:40 +0100

    commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'data.json', true);
+xhr.responseType = 'text';
+
+xhr.onload = function () {
+    if (xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText);
+        console.log(data);
+        // for (i = 0; i < data.article.length; i++) {
+        //     console.log(data.article[i].date);
+        // }
+        var dataString = "";
+        for (i = 0; i <data.article.length; i++) {
+            var counter = i + 1;
+            dataString += data.article[i].image + "";
+            dataString += data.article[i].button + " ";
+            dataString += data.article[i].date + ""+"<br>";
+            dataString += data.article[i].title + "";
+            dataString += "<br>";
+        }
+        document.getElementById('pdata').innerHTML = dataString;
+    }
+}
+
+xhr.send();
