7314d771ab41e06390e0522815db0620b58c7abb
rouamansour <rouamansour.rm@gmail.com>
2021-07-15 10:27:10 +0100

    commit

--- script.js
+++ script.js
@@ -6,17 +6,16 @@ xhr.onload = function () {
     if (xhr.status === 200) {
         var data = JSON.parse(xhr.responseText);
         console.log(data);
-        // for (i = 0; i < data.article.length; i++) {
-        //     console.log(data.article[i].date);
-        // }
         var dataString = "";
         for (i = 0; i <data.article.length; i++) {
             var counter = i + 1;
+            dataString +="<div class='design-item'><div class='design-img'>"
             dataString += data.article[i].image + "";
             dataString += data.article[i].button + " ";
             dataString += data.article[i].date + ""+"<br>";
             dataString += data.article[i].title + "";
             dataString += "<br>";
+            dataString += "</div>"+"</div>";
         }
         document.getElementById('pdata').innerHTML = dataString;
     }
