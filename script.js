43613da55f6c6f05662414602fa92b19c2f99e90
rouamansour <rouamansour.rm@gmail.com>
2021-07-22 16:00:10 +0100

    commit

--- script.js
+++ script.js
@@ -1,24 +1,60 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'data.json', true);
-xhr.responseType = 'text';
+//declaration des variables
+let id = [];
+let URLimage = [];
+let date = [];
+let button = [];
+let title = [];
+let i = 0;
+let structureArticle = "";
+//fonction pour affichage des articles
+function affichageArticle(response) {
+    const positionArticle = document.querySelector("#articleContent");
+    for (i = 0; i < response.length; i++) {
+        response.forEach((article, i) => {
+            id[i] = article.id;
+            URLimage[i] = article.URLimage;
+            date[i] = article.date;
+            button[i] = article.button;
+            title[i] = article.title;
+        });
+        structureArticle += `
+            <div class="design-item">
+                <div class="design-img">
+                    <a href="details.html?detail/id=${id[i]}">
+                    <img src="${URLimage[i]}">
+                    </a>
+                    <span>Dormal Corp</span>
+                </div>
+                <div class="design-title">
+                    ${button[i]} <span>${date[i]}</span> <br>
+                    <p>${title[i]}</p>
+                </div>
+            </div>  
+        `;
 
-xhr.onload = function () {
-    if (xhr.status === 200) {
-        var data = JSON.parse(xhr.responseText);
-        console.log(data);
-        var dataString = "";
-        for (i = 0; i <data.article.length; i++) {
-            var counter = i + 1;
-            dataString +="<div class='design-item'><div class='design-img'>"
-            dataString += "<a href='details.html'>"+data.article[i].image + ""+"</a>";
-            dataString += data.article[i].button + " ";
-            dataString += data.article[i].date + ""+"<br>";
-            dataString += data.article[i].title + "";
-            dataString += "<br>";
-            dataString += "</div>"+"</div>";
-        }
-        document.getElementById('pdata').innerHTML = dataString;
+        positionArticle.innerHTML = structureArticle;
+        //console.log(structureArticle);
     }
 }
+window.onload = () => {
+    affichageArticle(response);
+};
 
-xhr.send();
+
+
+
+ /*function getDataById(response, id) {
+   return response.id.find(a => a.id ===id);
+ }
+
+ const data = getDataById(response,id);
+ console.log(data);*/
+
+//methode fetch
+function fetchData(){
+    fetch("https://reqres.in/api/users?page=2");
+}
+
+fetchData();
+
+
